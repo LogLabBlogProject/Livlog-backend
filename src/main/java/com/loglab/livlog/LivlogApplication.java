7131de5e01a09073006f495385e63804c203fa6b
amuileobda <dev.ebosda000@gmail.com>
2025-09-09 01:25:55 +0900

    [chore] : 프로젝트 초기 설정

---
+++ src/main/java/com/loglab/livlog/LivlogApplication.java
@@ -0,0 +1,13 @@
+package com.loglab.livlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loglab/livlog/LivlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loglab.livlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
