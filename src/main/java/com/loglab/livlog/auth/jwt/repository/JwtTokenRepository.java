9fc12c55e99f8f5c967884f4d72eeacb910d43a3
rorrxr <rorrxr@naver.com>
2025-09-26 12:00:00 +0900

    [feat] : auth jwt 구현

---
+++ src/main/java/com/loglab/livlog/auth/jwt/repository/JwtTokenRepository.java
@@ -0,0 +1,16 @@
+package com.loglab.livlog.auth.jwt.repository;
+
+import com.loglab.livlog.auth.jwt.entity.JwtToken;
+import com.loglab.livlog.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface JwtTokenRepository extends JpaRepository<JwtToken, Long> {
+    Optional<JwtToken> findByUserAndDeviceInfo_DeviceId(User user, String deviceId);
+
+    boolean existsByUserId(Long userId);
+    boolean existsByUserIdAndDeviceInfo_DeviceId(Long userId, String deviceId);
+}
