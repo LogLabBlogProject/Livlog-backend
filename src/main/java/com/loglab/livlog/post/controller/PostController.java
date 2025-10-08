ed82d55717495c9663b1d9708eed0e4c35a0ae9a
amuileobda <dev.ebosda000@gmail.com>
2025-10-09 03:22:31 +0900

    [modify] : post code

--- src/main/java/com/loglab/livlog/post/controller/PostController.java
+++ src/main/java/com/loglab/livlog/post/controller/PostController.java
@@ -7,6 +7,8 @@ import com.loglab.livlog.post.dto.PostSimpleResponseDto;
 import com.loglab.livlog.post.dto.PostUpdateRequestDto;
 import com.loglab.livlog.post.service.PostReadService;
 import com.loglab.livlog.post.service.PostWriteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,44 +19,80 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Post API", description = "포스트 관련 CRUD API")
 public class PostController {
 
     private final PostReadService postReadService;
     private final PostWriteService postWriteService;
 
+    @Operation(
+            summary = "포스트ID로 상세 조회",
+            description = ""
+    )
     @GetMapping("/api/v1/post/detail/{id}")
     public ResponseEntity<CommonResponse<?>> getPost(@PathVariable Long id) {
         PostDetailResponseDto responseDto = postReadService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(responseDto));
     }
 
+    @Operation(
+            summary = "포스트 전체 조회",
+            description = ""
+    )
     @GetMapping("/api/v1/post/list")
     public ResponseEntity<CommonResponse<?>> getPosts() {
         List<PostSimpleResponseDto> responseDto = postReadService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(responseDto));
     }
 
+    @Operation(
+            summary = "포스트 검색",
+            description = ""
+    )
     @GetMapping("/api/v1/post/search")
     public ResponseEntity<CommonResponse<?>> searchPosts(@RequestBody Map<String, Object> params) {
         List<PostSimpleResponseDto> responseDto = postReadService.searchBy(params);
         return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(responseDto));
     }
 
+    @Operation(
+            summary = "포스트 생성",
+            description = ""
+    )
     @PostMapping("/api/v1/post/create")
     public ResponseEntity<CommonResponse<?>> createPost(@RequestBody PostCreateRequestDto requestDto) {
         PostDetailResponseDto responseDto = postWriteService.create(requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(responseDto));
     }
 
+    @Operation(
+            summary = "포스트 수정",
+            description = ""
+    )
     @PutMapping("/api/v1/post/update")
     public ResponseEntity<CommonResponse<?>> updatePost(@RequestBody PostUpdateRequestDto requestDto) {
         PostDetailResponseDto responseDto = postWriteService.update(requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(responseDto));
     }
 
+    @Operation(
+            summary = "포스트 삭제",
+            description = ""
+    )
     @DeleteMapping("/api/v1/post/delete/{id}")
     public ResponseEntity<CommonResponse<?>> deletePost(@PathVariable Long id) {
         PostDetailResponseDto responseDto = postWriteService.delete(id);
         return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(responseDto));
     }
+
+    @Operation(
+            summary = "유저ID로 유저 포스트 조회",
+            description = ""
+    )
+    @GetMapping("/api/v1/post/user/{userId}")
+    public ResponseEntity<CommonResponse<?>> getPostsByUser(@PathVariable Long userId) {
+        List<PostSimpleResponseDto> responseDto = postReadService.findByUserId(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(responseDto));
+    }
+
 }
--- src/main/java/com/loglab/livlog/post/dto/PostSimpleResponseDto.java
+++ src/main/java/com/loglab/livlog/post/dto/PostSimpleResponseDto.java
@@ -1,12 +1,15 @@
 package com.loglab.livlog.post.dto;
 
 import com.loglab.livlog.post.entity.Post;
+import com.loglab.livlog.tag.entity.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -17,12 +20,15 @@ public class PostSimpleResponseDto {
     private String title;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private Set<String> tagNames;
 
-    // Custom Constructor, convert data from entity to dto
     public PostSimpleResponseDto(Post entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.createdAt = entity.getCreatedAt();
         this.updatedAt = entity.getUpdatedAt();
+        this.tagNames = entity.getTags().stream()
+                .map(Tag::getName)
+                .collect(Collectors.toSet());
     }
 }
