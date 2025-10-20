326826852c177838865471eaf004bdc5eda7ded3
rorrxr <rorrxr@naver.com>
2025-10-20 12:00:00 +0900

    [feat] : 채팅 관련 repository 구현

--- src/main/java/com/loglab/livlog/chat/repository/ChatParticipantRepository.java
+++ src/main/java/com/loglab/livlog/chat/repository/ChatParticipantRepository.java
@@ -1,4 +1,11 @@
 package com.loglab.livlog.chat.repository;
 
-public interface ChatParticipantRepository {
+import com.loglab.livlog.chat.entity.ChatParticipant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
+    List<ChatParticipant> findByRoomId(Long roomId);
+    boolean existsByRoomIdAndMemberId(Long roomId, Long memberId);
 }
